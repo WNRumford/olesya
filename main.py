ece08862f56ce530dbf0b00322f82abe6563748d
Kirill <dukennt@gmail.com>
2018-07-21 20:56:49 +0000

    Натянул шаблон. Надо разобраться с сылками к static/, заменить фотки, заменить слайдер даже, а так норм

--- main.py
+++ main.py
@@ -14,11 +14,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    return render_template('index.html')
 
-@app.route('/bent')
-def bent():
-    return render_template('bent/bent.html')
 
 
 if __name__ == '__main__':
