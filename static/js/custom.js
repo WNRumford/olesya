f7e20542dfa60d644f5f6fa5f2afe95bc728543f
Kirill <dukennt@gmail.com>
2018-08-26 20:08:38 +0000

    header

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,7 +2,7 @@
 w3.slideshow(".dogimg", 3000);
 
 // mobile menu
-function myFunction() {
+function myMobileMenu() {
     var x = document.getElementById("demo");
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
@@ -65,5 +65,26 @@ function showDivs(n) {
   dots[slideIndex-1].className += " w3-opacity-off";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
-
 // ENDGallery
+
+// STICKY NAVBAR
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// END STICKY NAVBAR
