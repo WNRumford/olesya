9f7823037c2fce18e9a7c6858ec1d18d580859bd
Kirill <dukennt@gmail.com>
2018-09-03 21:17:26 +0000

    incapsulate tables, header and so on

--- static/js/custom.js
+++ static/js/custom.js
@@ -88,3 +88,14 @@ function myFunction() {
 }
 
 // END STICKY NAVBAR
+
+// цены на услуги
+function showPrice(id) {
+	var x = document.getElementById(id);
+	if (x.className.indexOf("w3-show") == -1) {
+		x.className += " w3-show";
+	} else {
+		x.className = x.className.replace(" w3-show", "");
+	}
+}
+// END цены на услуги
