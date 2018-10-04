3cceb759f3e3b54b2d20a3543e3ec188ea61a059
Kirill <dukennt@gmail.com>
2018-10-04 19:07:31 +0000

    на сервере не импортируется flask-mail, поравил в коде

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
 from funcs import get_data_form, write_json, read_json, get_check_psw
 import os
-from flask_mail import Mail, Message
+# from flask_mail import Mail, Message
 
 host = os.getenv('IP', '0,0,0,0')
 port = int(os.getenv('PORT', 8080))
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # Конфиги для почты
 
 # creating instance of Mail
-mail = Mail(app)
+# mail = Mail(app)
 
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/main', methods = ['GET', 'POST'])
