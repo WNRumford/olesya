f7b2e9f19852f54a174bbde00f6da3a777790396
Kirill <dukennt@gmail.com>
2018-10-13 20:39:04 +0000

    новая галлерея, подвал

--- static/js/custom.js
+++ static/js/custom.js
@@ -28,65 +28,27 @@ function faq(id) {
 
 // Gallery
 
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
 
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
-
-  }
-  x[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " w3-opacity-off";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
 // ENDGallery
 
 // STICKY NAVBAR
 
 // When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+window.onscroll = function(){stickyNavbar()};
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
+// get the navbar
+var navbar = document.getElementById("navbar")
 var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+// Add the sticky class to the navbar when you reach its scroll position. 
+// Remove "sticky" when you leave the scroll position
+function stickyNavbar(){
+  if (window.pageYOffset >= sticky){
+   navbar.classList.add("stickyy") 
   } else {
-    navbar.classList.remove("sticky");
+    navbar.classList.remove("stickyy");
   }
 }
-
 // END STICKY NAVBAR
 
 // цены на услуги
